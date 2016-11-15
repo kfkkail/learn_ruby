3d11860417e605605f157546cedb876662cae9a1
Kail Keusch <kfkkail@gmail.com>
2016-11-15 00:22:03 -0500

    first part of calculator completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+
+def add(a,b)
+  return a + b
+end
+
+def subtract(a,b)
+  return a - b
+end
+
+def sum(list)
+  if list.inject(:+) == nil
+    return 0
+  else
+    return list.inject(:+)
+  end
+end
