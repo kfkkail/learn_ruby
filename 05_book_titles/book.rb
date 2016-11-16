3d86f8a1b35bfe75d926f8643333cf808b19ba47
Kail Keusch <kfkkail@gmail.com>
2016-11-15 21:26:17 -0500

    test passing for book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(text)
+    splittedtext = text.split
+    capitalized_text = []
+    littleword = %w(over the and in of a an)
+    count = 0
+    splittedtext.each{|input|
+      count += 1
+      if ((littleword.include? input) && count != 1)
+        capitalized_text.push(input)
+      else
+        capitalized_text.push(input.capitalize)
+      end}
+    @title = capitalized_text.join(" ")
+  end
 end
