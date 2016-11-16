0abf09c84b1b009b2b8319aca413c9b96ae29ad9
Kail Keusch <kfkkail@gmail.com>
2016-11-15 20:57:55 -0500

    Completed pig latin!

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+def translate(str)
+  vowels = %w(a e i o u)
+  splitstr = str.split
+  words = []
+  splitstr.each {|input|
+    input = input.gsub("qu", "1")
+    if vowels.include? input[0]
+      # begins with vowel, just add "ay"
+      words.push(input+"ay")
+    elsif vowels.include? input[1]
+      words.push(input[1..input.length]+input[0]+"ay")
+    elsif vowels.include? input[2]
+      words.push(input[2..input.length]+input[0..1]+"ay")
+    else
+      words.push(input[3..input.length]+input[0..2]+"ay")
+    end
+  }
+  words.join(' ').chomp(' ').gsub("1", "qu")
+end
