7ee6bb4f82f859a9fe9924453442eee85e7b63ce
Kail Keusch <kfkkail@gmail.com>
2016-11-15 21:54:39 -0500

    Figured out a better way
    Didn’t like the code I wrote, so figure out a better way to do the
    timer function

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -11,35 +11,9 @@ class Timer
   def time_string
     # 3600 seconds in an hour
     # 60 seconds in a minute
+    hours = @seconds/3600
+    remainder = @seconds%3600
 
-    if @seconds > 3600
-      hour = @seconds/3600
-      remainder = @seconds - (hour*3600)
-      hour = hour.to_s
-      if hour.length < 2
-        hour = "0" + hour
-      end
-    else
-      remainder = @seconds
-      hour = "00"
-    end
-
-    if remainder > 60
-      minute = remainder/60
-      remainder = remainder - (minute*60)
-      minute = minute.to_s
-      if minute.length < 2
-        minute = "0" + minute.to_s
-      end
-    else
-      remainder = @seconds
-      minute = "00"
-    end
-
-    second = remainder.to_s
-    if second.length < 2
-      second = "0" + second
-    end
-    '' + hour + ':' + minute + ':' + second
+    sprintf('%02d:%02d:%02d', hours, remainder/60, remainder%60)
   end
 end
