c2d167c8f10aa6a28cfa917f4f42f4e1da29ca70
Kail Keusch <kfkkail@gmail.com>
2016-11-15 21:49:17 -0500

    Not the best looking code, but it passes

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,45 @@
 class Timer
-  #write your code here
+  attr_reader :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(time)
+    @seconds = time
+  end
+
+  def time_string
+    # 3600 seconds in an hour
+    # 60 seconds in a minute
+
+    if @seconds > 3600
+      hour = @seconds/3600
+      remainder = @seconds - (hour*3600)
+      hour = hour.to_s
+      if hour.length < 2
+        hour = "0" + hour
+      end
+    else
+      remainder = @seconds
+      hour = "00"
+    end
+
+    if remainder > 60
+      minute = remainder/60
+      remainder = remainder - (minute*60)
+      minute = minute.to_s
+      if minute.length < 2
+        minute = "0" + minute.to_s
+      end
+    else
+      remainder = @seconds
+      minute = "00"
+    end
+
+    second = remainder.to_s
+    if second.length < 2
+      second = "0" + second
+    end
+    '' + hour + ':' + minute + ':' + second
+  end
 end
